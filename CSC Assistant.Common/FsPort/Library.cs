b7bfd4eed5d1c0f3aa0f17a245ffd74f134b30ea
C3Bro <corruptingwind@gmail.com>
2020-12-14 18:52:01 +0200

    port complete
    but don't use the C# code, it's not good :P

--- CSC Assistant.Common/FsPort/Library.cs
+++ CSC Assistant.Common/FsPort/Library.cs
@@ -12,26 +12,52 @@ namespace CSC_Assistant.Common.FsPort
         // Not exactly the same as in F# but close enough in this case
         public enum ItemType { NFT, FT, Ore }
 
-        public static (ItemType, Dictionary<string, double>) ItemParts (Dictionary<string, Item> itemMap, string itemId )
+        public static (ItemType, Dictionary<string, double>) ItemParts(Dictionary<string, Item> itemMap, string itemId)
         {
             var data = itemMap[itemId].Blob.GameData;
-            Func<Resource, KeyValuePair<string, double>> splitLambda = 
+            Func<Resource, KeyValuePair<string, double>> splitLambda =
                 r => KeyValuePair.Create(r.ItemID, (double)r.Amount);
             if (data.CraftingResources != null && data.CraftingResources.Count > 0)
             {
                 var craftMap = new Dictionary<string, double>(data.CraftingResources.Select(splitLambda));
                 return (ItemType.NFT, craftMap);
-            }else if (data.RefinedResources != null && data.RefinedResources.Count > 0)
+            }
+            else if (data.RefinedResources != null && data.RefinedResources.Count > 0)
             {
                 var refineMap = new Dictionary<string, double>(data.RefinedResources.Select(splitLambda));
                 return (ItemType.FT, refineMap);
-            }else
+            }
+            else
             {
                 return (ItemType.Ore, new Dictionary<string, double>());
             }
         }
 
-        public static Dictionary<string, Item> ItemMap(IEnumerable<Item> allItems) => 
+        public static Dictionary<string, Item> ItemMap(IEnumerable<Item> allItems) =>
             new Dictionary<string, Item>(allItems.Select(i => KeyValuePair.Create(i.ItemId, i)));
+
+        public static Dictionary<string, double> mergeMaps(Dictionary<string, double> dict1, Dictionary<string, double> dict2) =>
+            new(dict1.Concat(dict2)
+                .GroupBy(kvp => kvp.Key)
+                .Select(grp => KeyValuePair.Create(
+                    grp.Key,
+                    grp.Select(g => g.Value).Sum())));
+
+        public static Dictionary<string, double> mergeMapSeq(IEnumerable<Dictionary<string, double>> arg) =>
+            arg.Aggregate(new Dictionary<string, double>(), (state, dict) => mergeMaps(state, dict));
+
+        public static Dictionary<string, double> DeeperParts(Dictionary<string, Item> itemMap, Dictionary<string, double> parts) =>
+            mergeMapSeq(
+                parts.Select(kvp =>
+                {
+                    var (typ, ps) = ItemParts(itemMap, "FT:" + kvp.Key);
+                    switch (typ)
+                    {
+                        case ItemType.Ore:
+                            return new Dictionary<string, double> { { kvp.Key, kvp.Value } };
+                        default:
+                            return new Dictionary<string, double>(ps.Select(p => KeyValuePair.Create(p.Key, p.Value * kvp.Value)));
+                    }
+                }));
     }
 }
